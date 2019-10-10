493219c48f14d90dec536c60fbc85547aaf8068d
Faith Janicek <faithe1937@gmail.com>
2019-10-10 22:01:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,11 @@
+#.each method
 def reverse_each_word(sentence)
   split_up_words = sentence.split("")
   reversed_sentence = split_up_words.each {|word| word.reverse!}
 return reversed_sentence.join("")
 end 
 
+#.collect method 
+def reverse_each_word(array)
+  new_array = array.split("")
+  reversed_array = new_array.collect {|x|x.reverse
